007171af0f9a39666a07895cbf3d0273c6436a65
awye765 <awye765@googlemail.com>
2016-12-30 18:32:59 +0000

    Initial commit: slider, basic buttons and labels added.  Slider functionality complete.

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -20,6 +20,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now now: \(slider.value)")
+    }
 }
 
