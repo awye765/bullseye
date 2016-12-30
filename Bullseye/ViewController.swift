9fb0ada652b577e71fa994ece99c4be2b41ab948
awye765 <awye765@googlemail.com>
2016-12-30 18:51:05 +0000

    Slider value now alerted to user when 'Hit Me' button clicked

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +23,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let message = "The value of the slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK",
+                                   style: .default,
+                                   handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        print("The value of the slider is now now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+        // lroundf rounds the float value of the slider to an integer
     }
 }
 
