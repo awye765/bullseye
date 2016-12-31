9d702c90d5a32900081ee1b19eec26c55e86d9b2
awye765 <awye765@googlemail.com>
2016-12-31 18:54:22 +0000

    Added 'about' page and transitions between about page and main page

---
+++ Bullseye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  Bullseye
+//
+//  Created by Alistair Wye on 31/12/2016.
+//  Copyright © 2016 wyedawg. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
