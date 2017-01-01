c9e6255252d56f89d569a905f285f33e6b6ec8ac
awye765 <awye765@googlemail.com>
2017-01-01 13:26:04 +0000

    Added crossfade transition

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -73,7 +74,6 @@ class ViewController: UIViewController {
         roundLabel.text = String(round)
     }
     
-    
     @IBAction func showAlert() {
         let difference = abs(currentValue - targetValue)
         
@@ -121,6 +121,14 @@ class ViewController: UIViewController {
     @IBAction func startOver() {
         startNewGame()
         updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseOut)
+        view.layer.add(transition, forKey: nil)
+        
     }
     
 }
